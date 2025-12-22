4707205f20939d5cea97218e27e22e6dfce4a048
Guilherme Vidichosqui Men <166948520+Guimenn@users.noreply.github.com>
2025-12-22 17:09:59 -0300

    asd

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,10 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   // Otimizações para SEO e Performance
   poweredByHeader: false,
   compress: true,
   generateEtags: true,
-  // Otimizações de performance
-  swcMinify: true,
   reactStrictMode: true,
   // Otimizar bundle
   experimental: {
